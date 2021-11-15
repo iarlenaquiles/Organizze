c1695985ea4a5744598e217ece5ec1f85e6cd7fd
Iarlen Aquiles <iarlensilva775@gmail.com>
2021-11-14 21:23:48 -0300

    user exceptions

--- app/src/main/java/com/aquiles/organizze/activity/CadastroActivity.java
+++ app/src/main/java/com/aquiles/organizze/activity/CadastroActivity.java
@@ -16,6 +16,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class CadastroActivity extends AppCompatActivity {
 
@@ -62,7 +65,21 @@ public class CadastroActivity extends AppCompatActivity {
                             Toast.makeText(CadastroActivity.this, "Sucesso ao cadastrar", Toast.LENGTH_LONG).show();
 
                         } else {
-                            Toast.makeText(CadastroActivity.this, "Erro ao cadastrar o usuário", Toast.LENGTH_LONG).show();
+                            String excecao = "";
+                            try {
+                                throw task.getException();
+                            }catch (FirebaseAuthWeakPasswordException e) {
+                                excecao = "Digite uma senha com letras e numeros";
+                            } catch (FirebaseAuthInvalidCredentialsException e ){
+                                excecao = "Por favor digite um e-mail válido";
+                            } catch (FirebaseAuthUserCollisionException e) {
+                                excecao = "essa conta ja foi cadastrada";
+                            } catch (Exception e) {
+                                excecao = "Erro ao cadastrar usuário " + e.getMessage();
+                                e.printStackTrace();
+                            }
+
+                            Toast.makeText(CadastroActivity.this, excecao, Toast.LENGTH_LONG).show();
                         }
 
                     }
